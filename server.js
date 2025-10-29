ebc9fc847438630b51d6e7b780a5928aedab163b
unknown <emmco96@gmail.com>
2025-10-29 14:09:46 +0100

    new

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ db.testConnection().then(() => {
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 1000, // limit each IP to 1000 requests per windowMs
+  windowMs: 15 * 60 * 1000, 
+  max: 1000, 
   message: 'Too many requests from this IP, please try again later.'
 });
 
